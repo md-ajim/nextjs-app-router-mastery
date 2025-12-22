abe120de48c698497e27cc7584e9556b76d78345
md-ajim <mdajim2669@gmail.com>
2025-12-22 23:51:50 +0600

    Add Card and Input components to event handling page
    Introduced Card and Input components from the UI library and defined a User interface for form state management. Updated the component to include an initial structure for the Card component. This sets the foundation for future enhancements to the event handling page.

--- my-app/components/typescript/evenhandleing.tsx
+++ my-app/components/typescript/evenhandleing.tsx
@@ -1,14 +1,46 @@
 "use clinet";
 import { Button } from "../ui/button";
+import { 
+  Card ,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardTitle,
+  CardHeader,
+  CardFooter
+
+
+
+} from "../ui/card";
+import { Input } from "../ui/input";
 import { useState } from "react";
 
-export default function EeventHandling() { 
+export default function EeventHandling() {
 
-  const [ counter , setCounter] = useState<number> (0)
+  interface User{
+    name : string,
+    age : number,
+    email : string,
+    password: string
+  }
+
+  const [ form , setForm] = useState<User>({
+    name: '',
+    age : 0,
+    email: '',
+    password : ''
+  })
+
+
 
   return <>
+
+        <Card>
+
+          
+        </Card>
   
-  
+
 
   </>;
 }
